97baf2684265fe5b2b0a728641e1b34094820108
vivek5633 <vivek.maurya@monkeyscience.in>
2024-05-25 11:28:30 +0000

    sfsd

--- functions/myfunction/src/main.js
+++ functions/myfunction/src/main.js
@@ -1,18 +1,53 @@
+import axios from 'axios';
 
-import axios from "axios";
+class Msg91 {
+    constructor(senderId, authKey) {
+        this.senderId = "MSCIENCE";
+        this.authKey = "422647AWRRh9VldHq6650826aP1";
+    }
 
-var options = {
-  method: 'POST',
-  url: 'https://control.msg91.com/api/v5/flow/',
-  headers: {
-    authkey: '422647AWRRh9VldHq6650826aP1',
-    accept: 'application/json',
-    'content-type': 'application/JSON'
-  }
-};
+    getName() {
+        return 'Msg91';
+    }
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
+    getMaxMessagesPerRequest() {
+        // TODO: Find real limit
+        return 1000;
+    }
+
+    async process(message) {
+        const to = message.getTo().map(to => ({ mobiles: to.replace('+', '') }));
+
+        const response = await axios.post('https://api.msg91.com/api/v5/flow/', {
+            sender: this.senderId,
+            otp: message.getContent(),
+            flow_id: message.getFrom(),
+            recipients: [to],
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'authkey': this.authKey,
+            }
+        });
+
+        return response.data;
+    }
+
+    async addTemplate(content, templateName, smsType = 'NORMAL') {
+        const response = await axios.post('https://control.msg91.com/api/v5/sms/addTemplate', {
+            template: content,
+            sender_id: this.senderId,
+            template_name: templateName,
+            smsType: smsType,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'authkey': this.authKey,
+            }
+        });
+
+        return response.data;
+    }
+}
+
+export default Msg91;
