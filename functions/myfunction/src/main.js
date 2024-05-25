11ae26ebd708495c8c721e3936e9c8c94618fcc5
vivek5633 <vivek.maurya@monkeyscience.in>
2024-05-25 11:21:56 +0000

    dfa

--- functions/myfunction/src/main.js
+++ functions/myfunction/src/main.js
@@ -1,35 +1,18 @@
-import axios from 'axios';
 
-// Define the function
-export async function sendSMSWithMsg91() {
-    const MSG91_API_KEY = '422647AWRRh9VldHq6650826aP1';
-    const MSG91_SENDER_ID = 'MSCIENCE';
-    const recipientPhoneNumber = '+919651260202'; // Replace with the recipient's phone number
+import axios from "axios";
 
-    const url = 'https://api.msg91.com/api/v5/flow/';
+var options = {
+  method: 'POST',
+  url: 'https://control.msg91.com/api/v5/flow/',
+  headers: {
+    authkey: '422647AWRRh9VldHq6650826aP1',
+    accept: 'application/json',
+    'content-type': 'application/JSON'
+  }
+};
 
-    const requestBody = {
-        flow_id: "66508446d6fc057e543529d2", // Assuming you have a template ID configured in your environment variables
-        sender: MSG91_SENDER_ID,
-        recipients: [
-            {
-                mobiles: recipientPhoneNumber,
-                VAR1: "Hey from is updated"
-            }
-        ]
-    };
-
-    try {
-        const response = await axios.post(url, requestBody, {
-            headers: {
-                'authkey': MSG91_API_KEY,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        return response.data;
-    } catch (error) {
-        console.error('Error sending SMS:', error);
-        throw error;
-    }
-}
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
