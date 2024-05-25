17e9285fd2b4683eb662acba737aafd761172878
vivek5633 <vivek.maurya@monkeyscience.in>
2024-05-25 11:34:13 +0000

    sfag

--- functions/myfunction/src/main.js
+++ functions/myfunction/src/main.js
@@ -1,53 +1,36 @@
-import axios from 'axios';
+import axios from "axios"
 
-class Msg91 {
-    constructor(senderId, authKey) {
-        this.senderId = "MSCIENCE";
-        this.authKey = "422647AWRRh9VldHq6650826aP1";
-    }
+// Define the function
+export async function sendSMSWithMsg91(name, phoneNumber) {
+    const MSG91_API_KEY = '422647AWRRh9VldHq6650826aP1';
+    const MSG91_SENDER_ID = 'MSCIENCE';
 
-    getName() {
-        return 'Msg91';
-    }
+    const url = 'https://api.msg91.com/api/v5/flow/';
 
-    getMaxMessagesPerRequest() {
-        // TODO: Find real limit
-        return 1000;
-    }
+    const requestBody = {
+        flow_id: "66508446d6fc057e543529d2", // Assuming you have a template ID configured in your environment variables
+        sender: MSG91_SENDER_ID,
+        recipients: [
+            {
+                mobiles: "+919651260202",
+                VAR1: "jesdfbnsdkjfbs",
+            }
+        ]
+    };
 
-    async process(message) {
-        const to = message.getTo().map(to => ({ mobiles: to.replace('+', '') }));
-
-        const response = await axios.post('https://api.msg91.com/api/v5/flow/', {
-            sender: this.senderId,
-            otp: message.getContent(),
-            flow_id: message.getFrom(),
-            recipients: [to],
-        }, {
+    try {
+        const response = await axios.post(url, requestBody, {
             headers: {
-                'Content-Type': 'application/json',
-                'authkey': this.authKey,
+                'authkey': MSG91_API_KEY,
+                'Content-Type': 'application/json'
             }
         });
 
         return response.data;
-    }
-
-    async addTemplate(content, templateName, smsType = 'NORMAL') {
-        const response = await axios.post('https://control.msg91.com/api/v5/sms/addTemplate', {
-            template: content,
-            sender_id: this.senderId,
-            template_name: templateName,
-            smsType: smsType,
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'authkey': this.authKey,
-            }
-        });
-
-        return response.data;
+    } catch (error) {
+        console.error('Error sending SMS:', error);
+        throw error;
     }
 }
 
-export default Msg91;
+
